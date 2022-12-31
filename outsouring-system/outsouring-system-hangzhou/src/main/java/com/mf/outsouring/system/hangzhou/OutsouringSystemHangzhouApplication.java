e782749de6ff3cc26fba47e1ffe8250fa370cf95
pingzhu <ping.zhu@microfocus.com>
2022-12-31 20:45:01 +0800

    feat: add outsouring system

---
+++ outsouring-system/outsouring-system-hangzhou/src/main/java/com/mf/outsouring/system/hangzhou/OutsouringSystemHangzhouApplication.java
@@ -0,0 +1,13 @@
+package com.mf.outsouring.system.hangzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsouringSystemHangzhouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsouringSystemHangzhouApplication.class, args);
+    }
+
+}
---
+++ outsouring-system/outsouring-system-hangzhou/src/test/java/com/mf/outsouring/system/hangzhou/OutsouringSystemHangzhouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mf.outsouring.system.hangzhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsouringSystemHangzhouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
