f88fb6071b0d015e157270e1bc697c5a8907ab9f
pingzhu <ping.zhu@microfocus.com>
2022-12-31 20:29:55 +0800

    feat: add http client code

--- customer-system/src/main/java/com/mf/projects/customer/system/controller/vo/CustomerStaffVO.java
+++ customer-system/src/main/java/com/mf/projects/customer/system/controller/vo/CustomerStaffVO.java
@@ -2,10 +2,14 @@ package com.mf.projects.customer.system.controller.vo;
 
 import com.mf.projects.customer.system.entity.staff.enums.Gender;
 import com.mf.projects.customer.system.entity.staff.enums.Status;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@Data
+@Accessors(chain = true)
 public class CustomerStaffVO {
     private Long groupId;
     private String nickname;
