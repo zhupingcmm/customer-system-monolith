6fd04e84fd7ff5850570327bffc538f8d4560a36
pingzhu <ping.zhu@microfocus.com>
2023-02-09 16:41:56 +0800

    feat: add log for filter

--- integration-service/integration-service-provider/src/main/java/com/mf/integration/service/provider/servicebus/filter/impl/CustomerStaffSpecialCharactersFilter.java
+++ integration-service/integration-service-provider/src/main/java/com/mf/integration/service/provider/servicebus/filter/impl/CustomerStaffSpecialCharactersFilter.java
@@ -2,12 +2,15 @@ package com.mf.integration.service.provider.servicebus.filter.impl;
 
 import com.mf.integration.service.provider.servicebus.filter.AbstractCustomerStaffFilter;
 import domain.PlatformCustomerStaff;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CustomerStaffSpecialCharactersFilter extends AbstractCustomerStaffFilter {
 
     @Override
     public PlatformCustomerStaff execute(PlatformCustomerStaff customerStaff) {
         if (customerStaff.getStaffName().matches("[\u4e00-\u9fa5]")) {
+            log.info("this staff is wrong [{}], and will be filtered", customerStaff.getStaffName());
             return null;
         }
         if (getNext() != null) {
